56e121dee1b26a0a2483207a11051301db33ee7f
kokchun <kokchungiang@gmail.com>
2025-11-08 16:17:15 +0100

    cleanup formatting

--- 13_subquery/sql/subquery.sql
+++ 13_subquery/sql/subquery.sql
@@ -1,44 +1,70 @@
 /* ===================
-   Single-row subquery 
-   =================== */
+Single-row subquery 
+=================== */
 -- the subquery returns one row
-
 -- filter students obtaining math grade higher than the average of all students
-select *
-from staging.students
-where grade_math > (select avg(grade_math) from staging.students) -- subquery in ()
-order by grade_math
-
+select
+   *
+from
+   staging.students
+where
+   grade_math > (
+      select
+         avg(grade_math)
+      from
+         staging.students
+   ) -- subquery in ()
+order by
+   grade_math;
 
 /* ===================
-   Multi-row subquery 
-   =================== */
+Multi-row subquery 
+=================== */
 -- the subquery returns mutiple rows
 -- often used with IN operator
-
 -- filter students in classes taught by Anna S.
-select *
-from staging.students
-where class_name IN  (select class_name from staging.teachers where teacher_name = 'Anna S.') -- subquery in ()
+select
+   *
+from
+   staging.students
+where
+   class_name IN (
+      select
+         class_name
+      from
+         staging.teachers
+      where
+         teacher_name = 'Anna S.'
+   );
 
-
+-- subquery in ()
 /* ===================
-   Correlated subquery 
-   =================== */
+Correlated subquery 
+=================== */
 -- the subquery depends on column values of the outer query
 -- the subquery is execuated per row of the outer query
-
 -- filter students obtaining math grade higher than the average of their own classes
-select *
-from staging.students s
-where s.grade_math > (select avg(grade_math) 
-                      from staging.students
-                      where class_name = s.class_name) -- subquery in ()
-order by s.class_name
+select
+   *
+from
+   staging.students s
+where
+   s.grade_math > (
+      select
+         avg(grade_math)
+      from
+         staging.students
+      where
+         class_name = s.class_name
+   ) -- subquery in ()
+order by
+   s.class_name;
 
 -- check the resutls of the above by looking at the avg grade per class
-select 
-  class_name,
-  avg(grade_math) as class_avg_grade
-FROM staging.students
-group by class_name
+select
+   class_name,
+   avg(grade_math) as class_avg_grade
+FROM
+   staging.students
+group by
+   class_name;
